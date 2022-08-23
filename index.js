c2889d524c2657667a1455e31d8b9910c4f55fc3
spur237 <samuel.nzioka@student.moringaschool.com>
2022-08-23 11:17:14 +0300

    done with assignments

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers
+      .filter(function (driver) {
+        return driver.toLowerCase().indexOf(name.toLowerCase()) !== -1;
+      })
+      .sort();
+  }
+  function fuzzyMatch(drivers, name) {
+    return drivers
+      .filter(function (driver) {
+        return driver.toLowerCase().indexOf(name.toLowerCase()) === 0;
+      })
+      .sort();
+  }
+  function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+  }
